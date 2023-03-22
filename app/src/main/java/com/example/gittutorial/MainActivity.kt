52649c42814762071214673763b62948035e4ae3
Vicente <dontomasgamer@gmail.com>
2023-03-22 22:13:25 +0100

    included print

--- app/src/main/java/com/example/gittutorial/MainActivity.kt
+++ app/src/main/java/com/example/gittutorial/MainActivity.kt
@@ -9,5 +9,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("Esto es un ejemplo")
+        println("Esto es un ejemplo")
+        println("Esto es un ejemplo")
+        println("Esto es un ejemplo")
+        println("Esto es un ejemplo")
+        println("Esto es un ejemplo")
     }
 }
